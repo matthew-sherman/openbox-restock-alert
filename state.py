b33059472da561673c4f7138ef02ceae67835c5d
Matthew <20729685+matthew-sherman@users.noreply.github.com>
2026-05-23 14:30:34 -0400

    Add has_alert_been_sent function to check alert state

--- state.py
+++ state.py
@@ -5,6 +5,10 @@ redis = Redis.from_env()
 STATE_KEY = "alert:sent"
 
 
+def has_alert_been_sent():
+    return redis.get(STATE_KEY) == "1"
+
+
 def set_alert_state(is_sent: bool):
     state = 1 if is_sent else 0
     redis.set(STATE_KEY, state)
