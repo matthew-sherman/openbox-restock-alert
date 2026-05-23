655cb0e9953a17e535f2ec55d99f27e722ec87e4
Matthew <20729685+matthew-sherman@users.noreply.github.com>
2026-05-23 09:00:33 -0400

    Integrate scraper with alerts in main

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from alerts import trigger_alert
+from scraper import check_stock_status
+
+
 def main():
-   print("Hello World!")
+    product = check_stock_status()
+
+    if not product or not product.get("name"):
+        print("Failed to retrieve product data. Skipping execution.")
+        return
+
+    if product.get("available"):
+        success_count, failure_count = trigger_alert(product)
+        print(f"{success_count} alerts sent.")
+        print(f"{failure_count} alerts failed to send.")
+    else:
+        print(f"{product.get('name')} is currently out of stock!")
+
 
 if __name__ == "__main__":
     main()
